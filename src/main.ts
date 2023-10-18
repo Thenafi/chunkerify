c4f2b005e2480f4bf7aa0361b8215d5d3daa4f44
Thenafi <65831153+Thenafi@users.noreply.github.com>
2023-10-18 18:30:53 +0600

    First Build

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 console.log("Built by The Nafi");
 
-const chunksize = Number(document.querySelector<HTMLInputElement>("#chunksize")?.value ?? 300);
+const chunksize = () => Number(document.querySelector<HTMLInputElement>("#chunksize")?.value ?? 300);
 
 const inputText = document.querySelector<HTMLTextAreaElement>("#InputText");
 
@@ -10,16 +10,26 @@ const chunkTheText = function (inputText: string, chunksize: number) {
 
     for (let i = 0; i < splittedArray.length; i++) {
         const singleWord = splittedArray[i];
+        if (singleWord.length > chunksize) {
+            window.alert("There is a word bigger than chunk Size")
+            return null
+        }
 
+        // to add the first chunk 
         if (finalArray.length === 0) {
             finalArray.push(singleWord);
-        } else if (finalArray.length !== 0 && finalArray[finalArray.length - 1].length < chunksize) {
+        }
+        //
+        else if (finalArray.length !== 0 && finalArray[finalArray.length - 1].length < chunksize - 10) {
             finalArray[finalArray.length - 1] += ` ${singleWord}`;
+            finalArray[finalArray.length - 1].trim()
         } else {
             finalArray.push(singleWord);
         }
     }
-
+    finalArray.forEach(element => {
+        console.log(element.length)
+    });
     return finalArray;
 };
 
@@ -41,11 +51,31 @@ const outputChunks = function (chunkArray: string[]) {
 
 }
 
+const copyTheText = () => {
+    document.querySelectorAll('p').forEach((element) => {
+        element.addEventListener("click", () => {
+            navigator.clipboard.writeText(element.innerText)
+        })
+    })
+}
+
+copyTheText()
+
 
 inputText?.addEventListener("input", () => {
-    const result = chunkTheText(inputText?.value ?? "", chunksize)
-    console.log(result);
-    outputChunks(result)
+    const result = chunkTheText(inputText?.value ?? "", chunksize())
+    outputChunks(result || ["Error"])
+    copyTheText()
+
+
+});
+
+
+
+navigator.permissions.query({ name: "clipboard-write" as PermissionName }).then((result) => {
+    if (result.state == "granted" || result.state == "prompt") {
+        console.log("Write access granted!");
+    }
 });
 
 
